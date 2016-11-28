de06fc8e8fdd427f8153410b5d1489952eee4024
laurent <laurent.novac@garagecube.com>
2016-11-28 19:07:45 +0100

    remove trait code, too complicated for the focus of the course and not so relevant + time based euler

--- GenericParticleSystem/Qt/GenericParticleSystemQtApp/mainwindow.h
+++ GenericParticleSystem/Qt/GenericParticleSystemQtApp/mainwindow.h
@@ -8,6 +8,7 @@
 #include "IntegrationPolicy.h"
 #include "ParticleTraits.h"
 #include <QMainWindow>
+#include <QTime>
 
 
 using ParticleType = vac::Particle<QVector2D>;
@@ -36,7 +37,7 @@ namespace Ui {
 class MainWindow;
 }
 
-const int PARTICLE_COUNT = 100;
+const int PARTICLE_COUNT = 500;
 class QGraphicsEllipseItem;
 class QGraphicsScene;
 class MainWindow : public QMainWindow
@@ -53,6 +54,8 @@ private:
     ParticleSystemType m_pSystem;
     vac::ParticleRenderer m_renderer;
     QTimer *timer;
+    QTime m_time;
+
     QGraphicsScene *m_scene;
     QVector<QGraphicsEllipseItem*> m_ellipses;
     void initEventLoop();
--- GenericParticleSystem/src/IntegrationPolicy.h
+++ GenericParticleSystem/src/IntegrationPolicy.h
@@ -1,20 +1,18 @@
 #pragma once
 
 #include "ParticleTraits.h"
-
+//http://gafferongames.com/game-physics/integration-basics/
 namespace vac {
 struct EulerPolicy {
     template <typename ParticleType>
-    static void integrate(ParticleType& p)
+    static void integrate(ParticleType& p, double dt)
     {
         auto& pos = traits::access<ParticleType>::get_pos(p);
         auto& vel = traits::access<ParticleType>::get_vel(p);
         auto& acc = traits::access<ParticleType>::get_acc(p);
 
-        vel += acc;
-        pos += vel;
-
-        acc *= 0;
+        pos += vel*dt;
+        vel += acc * dt;
     }
 };
 }
